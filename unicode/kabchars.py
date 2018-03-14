ffb6acb1becb2ad4d09b137de54fe1bf4998fb4d
Muḥend Belqasem <belkacem77@gmail.com>
2018-03-14 20:20:29 +0100

    Update kabchars.py

--- unicode/kabchars.py
+++ unicode/kabchars.py
@@ -1,2 +1,2 @@
-s="abcdefghijklmnopqrstuvwxyzčḍǧḥṛṣṭẓɣɛABCDEFGHIJKLMNOPQRSTUVWXYZČḌǦḤṚṢṬƔƐ-.,;:/\!+%()[]{}_= ?!»«'\"³²&@#0123456789"
+s="abcdefghijklmnopqrstuvwxyzčḍǧḥṛṣṭẓɣɛABCDEFGHIJKLMNOPQRSTUVWXYZČḌǦḤṚṢṬƔƐ-.,;:/\!+*%()[]{}_= ?!»«'\"³²&@#0123456789$€£µ"
 for _c in s: print('U+%04x' % ord(_c),'---->',_c)
