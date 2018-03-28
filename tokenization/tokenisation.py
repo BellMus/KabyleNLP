a4c339b9051a54e5a2672cecbe18c570f51b33a5
Muḥend Belqasem <belkacem77@gmail.com>
2018-03-28 21:09:13 +0100

    Update tokenisation.py

--- tokenization/tokenisation.py
+++ tokenization/tokenisation.py
@@ -1,26 +1,78 @@
-affixe=[]
+suffixe=[]
+prefixe=[]
 
-Ponctuation=['.',',',';','?','!',':','"','(',')']
+Ponctuation=['.',',',';','?','!',':','"','(',')','*','_']
+#Construct suffixes and prefixes dictionaries
 for i in open("c:/tal/affixescolles.txt",encoding='utf-8'):
     a=i
     a=a.replace("\ufeff","")
     a=a.replace("\n","")
-    affixe.append(str(a))
+    if (a[len(a)-1]=="-"):
 
-for line in open("c:/tal/corpusliterraire.txt",encoding='utf-8'):
-    for i in Ponctuation:
-        line=line.replace(i,' '+i)
-    for i in affixe:
-        if (i[0]=='-'):
-            line=line.replace(i,' '+i)
+     prefixe.append(str(a))
+    else:
+       suffixe.append(str(a))
+#tokinize a token separated by spaces, exract unique morphemes : affixes and word affixed
+def tokenize_word(word,suffixe,prefixe):
+    a=''
+    morpheme=word[0:word.find('-')+1]
+
+    lemtized_word=' '
+    if (morpheme in prefixe):
+        word=word[word.find('-')+1:len(word)]
+        lemtized_word=lemtized_word+' '+morpheme
+        while word.find('-')>=0:
+
+            morpheme=word[0:word.find('-')+1]
+            word=word[word.find('-')+1:len(word)]
+            lemtized_word=lemtized_word+' '+morpheme
+        lemtized_word=lemtized_word+' '+word
+    else:
+        morpheme=word[0:word.find('-')]
+        lemtized_word=lemtized_word+' '+morpheme
+        word=word[word.find('-')+1:len(word)]
+        while word.find('-')>=0:
+
+           morpheme=word[0:word.find('-')]
+           lemtized_word=lemtized_word+' '+'-'+morpheme
+           word=word[word.find('-')+1:len(word)]
+        lemtized_word=lemtized_word+' '+'-'+word
+
+
+
+    return lemtized_word
+
+# Lematize a sentence
+def tokenize(sentence,suffixe,prefixe):
+       a=sentence.split()
+       sentence1=""
+       for i in a: #mots
+        if(i.find('-')<0):
+            sentence1=sentence1+' '+i
         else:
-            line=line.replace(i,i+' ')
-
-    line=line.replace("  "," ")
-    print (line)
-
+            words=tokenize_word(i,suffixe,prefixe)
+            sentence1=sentence1+' '+words
+       return sentence1
 
 
+text=""
 
+f= open("c:/tal/mhenna_tokenized.txt","w+",encoding='utf-8') #file tokennized
+g=open("c:/tal/mhenna.txt",encoding='utf-8') { file to tokennize
+for line in g:
+    #put ponctuaction between spaces
+    for i in Ponctuation:
 
+        if i=='.':
+            line=line.replace(i,' '+i+' ')
+        else:
+            line=line.replace(i,' '+i+' ')
 
+    ligne=tokenize(line,suffixe,prefixe)
+
+    ligne=ligne.replace("  "," ")# delete double spaces
+    ligne=ligne+'\n' # add the line end
+    f.write(ligne)
+
+f.close()
+g.close()
